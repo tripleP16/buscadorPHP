546b2225ee40c342593e8569cdc2c6febea3bafc
Pablo Perez <perez51160900@gmail.com>
2020-04-02 11:36:49 -0400

    ff

--- js/index.js
+++ js/index.js
@@ -13,7 +13,27 @@ $.fn.scrollEnd = function(callback, timeout) {
 /*
   Función que inicializa el elemento Slider
 */
-
+function init(){
+  var tipos = [];
+  var ciudades = [];
+  $.get('data-1.json', function(data){
+      for(let i = 0; i < data.length; i++){
+          if(tipos.indexOf(data[i].Tipo) === -1) tipos.push(data[i].Tipo);
+          if(ciudades.indexOf(data[i].Ciudad) === -1) ciudades.push(data[i].Ciudad);
+      }
+      for(let i = 0; i < ciudades.length; i++){
+          $('#selectCiudad').append('<option value="'+ciudades[i]+'">'+ciudades[i]+'</option>');
+      }
+      for(let j = 0; j < tipos.length; j++){
+          $('#selectTipo').append('<option value="'+tipos[j]+'">'+tipos[j]+'</option>');
+      }
+      $('select').material_select();
+  });
+}
+$(document).ready(function(){
+  inicializarSlider();
+  init();
+});
 function inicializarSlider(){
   $("#rangoPrecio").ionRangeSlider({
     type: "double",
@@ -28,7 +48,7 @@ function inicializarSlider(){
 /*
   Función que reproduce el video de fondo al hacer scroll, y deteiene la reproducción al detener el scroll
 */
-function playVideoOnScroll(){
+/*function playVideoOnScroll(){
   var ultimoScroll = 0,
       intervalRewind;
   var video = document.getElementById('vidFondo');
@@ -46,7 +66,39 @@ function playVideoOnScroll(){
     .scrollEnd(()=>{
       video.pause();
     }, 10)
-}
+}*/
 
 inicializarSlider();
-playVideoOnScroll();
+//playVideoOnScroll();
+
+function showResult(array){
+  $('.resultados').empty();
+  for(let i=0; i<array.length; i++){
+      $('.resultados').append(`<div class="card horizontal">
+          <div class="card-image place-wrapper">
+              <img class="img-responsive place-image" src="img/home.jpg" >
+          </div>
+          <div class="card-stacked">
+              <div class="card-content">
+                  <p>
+                      <b>Dirección: </b>${array[i].Direccion}<br>
+                      <b>Ciudad: </b>${array[i].Ciudad}<br>
+                      <b>Teléfono: </b>${array[i].Telefono}<br>
+                      <b>Código Postal: </b>${array[i].Codigo_Postal}<br>
+                      <b>Tipo: </b>${array[i].Tipo}<br>
+                      <span class="price"><b>Precio: </b>${array[i].Precio}</span>
+                  </p>
+              </div>
+              <div class="card-action">
+                  <a>Ver mas</a>
+              </div>
+          </div>
+      </div>`);
+  }
+}
+
+$('#mostrarTodos').click(function(){
+  $.get('data-1.json', function(data){
+    showResult(data);
+  });
+})
