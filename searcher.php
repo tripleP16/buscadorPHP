f78c9565ef8a955fe254380434841d351bdd36ae
Pablo Perez <perez51160900@gmail.com>
2020-04-02 17:12:32 -0400

    gggg

--- searcher.php
+++ searcher.php
@@ -1,6 +1,4 @@
 <?php
-//Archivo php para llamada AJAX. 
-require ('C_place.php');
 $ciudad = $_GET['ciudad'];
 $precio =  htmlspecialchars($_GET['precio']);
 $tipo = $_GET['tipo'];
@@ -12,10 +10,18 @@ $file = fopen("data-1.json", "r") or die("No se puede abrir el archivo");
 $json = fread($file, filesize('data-1.json'));
 $data = json_decode($json, true);
 $datos = array(); 
+$places = array();
 foreach ($data as $place) {
-    array_push($datos, $place);
+    $precio = $place['Precio'];
+    $precio = substr($precio, strpos($precio,'$')+1);
+    $aux = strpos($precio,',');
+    $precio = substr($precio,0,$aux).substr($precio,$aux+1);
+    if($precio>=$min && $precio<=$max){
+        array_push($datos, $place);
+    }
     
 }
+
 $result = array();
 if(!empty($ciudad) && !empty($tipo)){
     foreach($datos as $dato){
@@ -25,6 +31,22 @@ if(!empty($ciudad) && !empty($tipo)){
         }
     }
 
+}elseif(!empty($ciudad)){
+    foreach($datos as $dato){
+        if($dato['Ciudad']==$ciudad){
+            array_push($result, $dato);
+            
+        }
+    }
+}elseif(!empty($tipo)){
+    foreach($datos as $dato){
+        if($dato['Tipo']==$tipo){
+            array_push($result, $dato);
+            
+        }
+    }
+}else{
+    $result = $datos;
 }
 
 $rjson = json_encode($result);
